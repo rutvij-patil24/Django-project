0bb6e3bf4a8ba9d4deae9de343a0aa45e70455a5
Rutvij Patil <66994155+rutvij-patil24@users.noreply.github.com>
2021-02-22 14:51:14 +0530

    Added Template
    Used templates to make the Blog website more user friendly. Imported a bootstrap starter template and made it as a parent template. Then used some code snippets to modify home and about page. Made static references to css file main.css & also gave static url to home and about page which is a better way than hard coding hrefs, so that we won't have to make changes in multiple places fo a single change.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# dummy data (list of dictionaries)
+posts = [
+
+	{
+		'author':'Rutvij Patil',
+		'title':'My First Blog',
+		'content':'In this blog I will be talking about Wordpress',
+		'date_posted':'February 24, 2020'
+	},
+	{
+		'author':'Rick Sanchez',
+		'title':'Impact of 5G on humans',
+		'content':'In this blog I will be talking about 5G',
+		'date_posted':'March 1, 2020'
+	}
+
+]
+
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	# return HttpResponse('<h1>Blog Home</h1>')
+
+	context = {
+		'posts':posts
+	} # assigned list of posts as value to the key 'posts'
+
+	return render(request,'blog/home.html',context)
+	# render function returns http response
 
 
 def about(request):
-	return HttpResponse('<h1>About this Blog Website</h1>')
+	# return HttpResponse('<h1>About this Blog Website</h1>')
+	return render(request,'blog/about.html',{'title':'About'})
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig', # adding a new application
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
