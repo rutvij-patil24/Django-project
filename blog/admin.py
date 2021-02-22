8ac544dbb7e6a5d452d472961fd7d8bcca82c32a
Rutvij Patil <66994155+rutvij-patil24@users.noreply.github.com>
2021-02-22 19:21:52 +0530

    Created Database and Migrated
    Created a Post database with attributes of title, author, content and date posted. Registered the new database in Django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+admin.site.register(Post) # to register new post database into django admin
+
+
+
+
+
+
+
+
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Post
 
 # dummy data (list of dictionaries)
-posts = [
+# posts = [
 
-	{
-		'author':'Rutvij Patil',
-		'title':'My First Blog',
-		'content':'In this blog I will be talking about Wordpress',
-		'date_posted':'February 24, 2020'
-	},
-	{
-		'author':'Rick Sanchez',
-		'title':'Impact of 5G on humans',
-		'content':'In this blog I will be talking about 5G',
-		'date_posted':'March 1, 2020'
-	}
+# 	{
+# 		'author':'Rutvij Patil',
+# 		'title':'My First Blog',
+# 		'content':'In this blog I will be talking about Wordpress',
+# 		'date_posted':'February 24, 2020'
+# 	},
+# 	{
+# 		'author':'Rick Sanchez',
+# 		'title':'Impact of 5G on humans',
+# 		'content':'In this blog I will be talking about 5G',
+# 		'date_posted':'March 1, 2020'
+# 	}
 
-]
+# ]
 
 
 def home(request):
 	# return HttpResponse('<h1>Blog Home</h1>')
 
+	# context = {
+	# 	'posts':posts
+	# } # assigned list of posts as value to the key 'posts'
+
 	context = {
-		'posts':posts
-	} # assigned list of posts as value to the key 'posts'
+		'posts': Post.objects.all()
+	}
 
 	return render(request,'blog/home.html',context)
 	# render function returns http response
